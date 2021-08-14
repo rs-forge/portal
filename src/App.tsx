4073cb2ea36bce901e0597a98e370a35a6c6bf13
Jake Lewis <jake@jakes-mbp.home>
2021-08-14 21:53:39 +0100

    init firebase test

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
+import firebase from 'firebase';
 import './App.css';
 import { useGetMembersQuery } from './services/members';
 
-function App() {
-  const { data, isError, isLoading, error } = useGetMembersQuery({});
-
+export default function App() {
+  const firebaseApp = firebase.apps[0];
   return (
-    <div className="App">
-      {isError ? (
-        <>Oh no, there was an error: {error}</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <>
-          <h3>Members</h3>
-          {data.members.map(member => 
-            <div>
-              <p>Name: {member.name}</p>
-              <ul>
-                {member.instruments.map(instrument =>
-                  <li>{instrument.valueOf()}</li>
-                )}
-              </ul>
-            </div>
-          )}
-        </>
-      ) : null}
+    <div>
+      <h1>React & Firebase Test</h1>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
     </div>
   );
 }
 
-export default App;
+// function App() {
+//   const { data, isError, isLoading, error } = useGetMembersQuery({});
+
+//   return (
+//     <div className="App">
+//       {isError ? (
+//         <>Oh no, there was an error: {error}</>
+//       ) : isLoading ? (
+//         <>Loading...</>
+//       ) : data ? (
+//         <>
+//           <h3>Members</h3>
+//           {data.members.map(member => 
+//             <div>
+//               <p>Name: {member.name}</p>
+//               <ul>
+//                 {member.instruments.map(instrument =>
+//                   <li>{instrument.valueOf()}</li>
+//                 )}
+//               </ul>
+//             </div>
+//           )}
+//         </>
+//       ) : null}
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,17 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import firebase from "firebase/app";
+
+firebase.initializeApp ({
+  apiKey: "AIzaSyBTFH6AtPzoOREIOOYubqketPvnYGrUJtg",
+  authDomain: "rs-forge.firebaseapp.com",
+  projectId: "rs-forge",
+  storageBucket: "rs-forge.appspot.com",
+  messagingSenderId: "635514687335",
+  appId: "1:635514687335:web:d524f5f8c7c6d7d9a2a9c2",
+  measurementId: "G-JQ37TXVQDH"
+});
 
 ReactDOM.render(
   <React.StrictMode>
